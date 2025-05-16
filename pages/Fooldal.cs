a888754498441e9c0e929e95346d64e2c6e5bc92
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-05-16 20:58:15 +0200

    Kész a főoldal

--- pages/Fooldal.cs
+++ pages/Fooldal.cs
@@ -12,9 +12,89 @@ namespace Szoftverteszt_beadando.pages
     public class Fooldal : Base_oldal
     {
         public static readonly string URL = "http://127.0.0.1:5500/fefedezo_appocska/fooldal.html";
+        ReadOnlyCollection<IWebElement> termekek => driver.FindElements(By.ClassName("termek-kartya"));
+        ReadOnlyCollection<IWebElement> termekarak => driver.FindElements(By.ClassName("termek-ar"));
+        ReadOnlyCollection<IWebElement> termeknevek => driver.FindElements(By.ClassName("termek-nev"));
+        ReadOnlyCollection<IWebElement> addButtons => driver.FindElements(By.Id("hozzaadas"));
+        ReadOnlyCollection<IWebElement> kedvencekButtons => driver.FindElements(By.Id("kedvencek"));
+        IWebElement TermekekListazva => driver.FindElement(By.Id("termekLista"));
+        IWebElement BeallitasoldalGomb => driver.FindElement(By.Id("beallitasok"));
+        IWebElement EltuntetesGomb => driver.FindElement(By.Id("toggleTermekek"));
+        IWebElement Alert => driver.FindElement(By.ClassName("alert"));
         public Fooldal(IWebDriver driver) : base(driver)
         {
-            
+
+        }
+
+        public int GetTermekSzama()
+        {
+            return termekek.Count;
+        }
+
+        public bool LatszanakeaTermekek()
+        {
+            try
+            {
+                return TermekekListazva.Displayed;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+        }
+
+        public void AddTermekToBeallitasok(int index)
+        {
+            if (index < addButtons.Count)
+            {
+                addButtons[index].Click();
+            }
+        }
+
+        public void AddTermekToKedvencek(int index)
+        {
+            if (index < kedvencekButtons.Count)
+            {
+                kedvencekButtons[index].Click();
+            }
+        }
+
+        public void GoToBeallitasok()
+        {
+            BeallitasoldalGomb.Click();
+        }
+
+        public void ShowOrNotShowTermekek()
+        {
+            EltuntetesGomb.Click();
+        }
+
+        public bool IsTermekAddedtoBeallitasokAlertShows()
+        {
+            try
+            {
+
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+
+                return false;
+            }
+        }
+
+        public bool IsTermekAddedtoKedvencekAlertShows()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
         }
     }
 }
---
+++ tests/FoOldalTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+
+namespace Szoftverteszt_beadando.tests
+{
+    [TestFixture]
+    internal class FoOldalTests : BaseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            login_Oldal.LoggingIn("tesztuser", "titkos");
+        }
+
+        [Test]
+        public void TermekekListajaDisplayedCorrectly()
+        {
+            Assert.AreEqual(6, fo_Oldal.GetTermekSzama());
+        }
+
+        [Test]
+        public void BeallitasokGombNavigatesToSettingsPerfectly()
+        {
+            fo_Oldal.GoToBeallitasok();
+            //Assert.AreEqual(Beallitasok_oldal.URL, driver.Url);
+        }
+
+        [Test]
+        public void TermekekDissappearCorrectly()
+        {
+            fo_Oldal.ShowOrNotShowTermekek();
+            Assert.False(fo_Oldal.LatszanakeaTermekek());
+        }
+
+        [Test]
+        public void TermekekAppearCorrectly() 
+        {
+            if(fo_Oldal.LatszanakeaTermekek() == false)
+            {
+                fo_Oldal.ShowOrNotShowTermekek();
+            }
+        }
+
+        [Test]
+        public void IsTermekAddedtoSettingsandAlertShows()
+        {
+            fo_Oldal.AddTermekToBeallitasok(2);
+            Assert.True(fo_Oldal.IsTermekAddedtoBeallitasokAlertShows());
+
+        }
+
+        [Test]
+        public void IsTermekAddedtoKedvencekandAlertShows()
+        {
+            fo_Oldal.AddTermekToKedvencek(4);
+            Assert.True(fo_Oldal.IsTermekAddedtoKedvencekAlertShows());
+        }
+    }
+}
